c12531ffa85533b30ce54c33a3c52206385b6081
arshal <arshaljo24@gmail.com>
2023-06-01 16:44:17 +0530

    upto view records

--- website/models.py
+++ website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+     created_at = models.DateTimeField(auto_now_add=True)
+     firstname =models.CharField(max_length=50)
+     lastname = models.CharField(max_length=50)
+     email =models.CharField(max_length=100)
+     phone = models.CharField(max_length=20)
+     address = models.CharField(max_length=100)
+     city = models.CharField(max_length=30)
+     state = models.CharField(max_length=30)
+     zipcode = models.CharField(max_length=10)
+
+
+     def __str__(self):
+         return (f"{self.firstname} {self.lastname}")
+
+
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
+from .forms import SignUpForm
+from .models import Record
 
 def index(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +19,7 @@ def index(request):
             messages.success(request,"Invalid username or password combination")
             return redirect('index')
     else:
-        return render(request, 'index.html', {})
+        return render(request, 'index.html', {'records' : records})
 
 
 def login_user(request):
@@ -31,4 +33,18 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have successfully registered")
+            return redirect('index')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
